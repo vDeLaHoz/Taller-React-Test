c2cf2ab90529590ba4f15fa3011400823ed50bf6
Victor De La Hoz <victordelahoz13@gmail.com>
2022-06-03 20:39:11 -0500

    Commit inicial

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import logo from './logo.png'
+import { useState } from 'react'
+import React from "react"
+import { Formulario } from './Componentes/form';
+import { Contenedor } from './Componentes/contenedor'
+import {firebase} from './firebase'
+
+const App = ({categorias = []}) => {
+  const [categoriasBusqueda, setCategoriasBusqueda] = useState(categorias);
+  const [lista, setLista] = useState([])
+
+  React.useEffect(()=>{
+    const obtenerDatos = async () =>{
+        try{
+            const db = firebase.firestore()
+            const data = await db.collection('pokemones').get()
+            const array = data.docs.map(item =>(
+                {
+                    id:item.id, ...item.data()
+                }
+            ))
+            setLista(array)
+
+        }catch(error){
+            console.log(error)
+        }
+    }
+    obtenerDatos()
+}, [lista])
+
+  return (
+    <div className="App">
+        <img src={logo} alt='poke' className='poke'/>
+        <hr className='hr'/>
+        <Formulario setCategoriasBusqueda={setCategoriasBusqueda}/>
+        <ol>
+        {
+            lista.map(item => (
+                <Contenedor
+                    key = {item.id} 
+                    valorBusqueda={item}
+                />
+            ))
+        }
+    </ol>
+    </div>
+  );
+}
+
+export default App;
